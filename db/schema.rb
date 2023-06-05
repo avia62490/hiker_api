c8dfc4c7cbd04abbea8b2ae700fd9272fe63bcc9
Alex Via <alexandervia@gmail.com>
2023-06-05 12:46:04 -0500

    adds HikeRating model with associations to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_171004) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_173850) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hike_ratings", force: :cascade do |t|
+    t.integer "hike_id"
+    t.integer "user_id"
+    t.integer "rating"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
