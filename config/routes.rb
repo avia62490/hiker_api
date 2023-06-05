6924718b51874ac9af309666ab34798bc0fd4a1a
michaelrmartin <iammichaelmartin@gmail.com>
2023-06-05 12:21:30 -0500

    Adds index and show routes for hikes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Hikes routes
+
+  get "/hikes" => "hikes#index"
+  get "/hikes/:id" => "hikes#show"
+  
 end
