ee4ea04bdecf5d3c78f78258bef29d37d1d08068
michaelrmartin <iammichaelmartin@gmail.com>
2023-06-05 12:50:34 -0500

    Adds show and index actions to hikes controller

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,2 +1,17 @@
 class HikesController < ApplicationController
+
+  def index
+    hikes = Hike.all
+
+    render json: hikes.as_json
+  end
+
+  def show
+    hike_id = params["id"]
+    hike = Hike.find_by(id: hike_id)
+
+    render json: hike.as_json 
+
+  end
+
 end
