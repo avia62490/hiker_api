ab450ad68e3b977f7571979bb69348746a0e20b7
JRisJunior <jeffmrubin@gmail.com>
2023-06-21 12:00:56 -0500

    changes inc

--- db/schema.rb
+++ db/schema.rb
@@ -25,15 +25,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_171154) do
     t.string "lon"
     t.string "difficulty"
     t.string "thumbnail"
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_173850) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "hike_ratings", force: :cascade do |t|
-    t.integer "hike_id"
-    t.integer "user_id"
-    t.integer "rating"
-    t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
