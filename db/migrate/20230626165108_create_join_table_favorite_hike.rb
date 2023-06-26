4d39aeedd51c9e19c07ad219e09a0f5f03257b9a
JRisJunior <jeffmrubin@gmail.com>
2023-06-26 11:53:12 -0500

    create fav hike join table using user and hike models

---
+++ db/migrate/20230626165108_create_join_table_favorite_hike.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableFavoriteHike < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :hikes, :users do |t|
+      # t.index [:hike_id, :user_id]
+      # t.index [:user_id, :hike_id]
+    end
+  end
+end
