fae0f1b6ee4a38084ef0fb75bab6548241d8214a
Alex Via <alexandervia@gmail.com>
2023-07-11 16:19:54 -0500

    adds RESTful dynamic favorite_hikes routes and functions

---
+++ app/controllers/favorite_hikes_controller.rb
@@ -0,0 +1,28 @@
+class FavoriteHikesController < ApplicationController
+
+  def index
+    favorite_hikes = FavoriteHike.all
+    render json: favorite_hikes.as_json
+  end
+
+  def show
+    favorite_hike = FavoriteHike.find_by(id: params[:id])
+    render json: favorite_hike.as_json
+  end
+
+  def create
+    favorite_hike = FavoriteHike.new(
+      hike_id: params[:hike_id],
+      user_id: params[:user_id]
+    )
+    favorite_hike.save
+    render json: favorite_hike.as_json
+  end
+
+  def destroy
+    favorite_hike = FavoriteHike.find_by(id: params[:id])
+    favorite_hike.destroy
+    render json: {message: "Favorite hike removed"}
+  end
+
+end
